4fc312c14e67344230f17eca7921dec8f013806b
abhirup2565 <abhirup2565@gmail.com>
2025-08-14 16:29:37 +0530

    added templates,css and bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'CoreyMs',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'Aug 14,2025'
+    },
+    {
+        'author':'Abhirup',
+        'title':'Blog post 2',
+        'content':'First post content',
+        'date_posted':'Aug 14,2025'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request,'blog/about.html',{'title':'Django-About'})
