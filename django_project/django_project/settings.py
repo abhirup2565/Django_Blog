3b8f98734de6b0b0f15dcc3a5d512a2c954eff27
abhirup2565 <abhirup2565@gmail.com>
2025-08-18 12:42:59 +0530

    login logout feature

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -126,3 +126,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home' 
+LOGIN_URL = 'login' 
